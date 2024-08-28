4184944461a5078da999edae58126600bc0eef86
wungjyan <1170017412@qq.com>
2024-08-28 17:29:53 +0800

    feat: 自定义异常捕获

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { FormatResponseInterceptor } from './format-response.interceptor';
 import { InvokeRecordInterceptor } from './invoke-record.interceptor';
+import { CustomExceptionFilter } from './custom-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new FormatResponseInterceptor())
   app.useGlobalInterceptors(new InvokeRecordInterceptor())
+  app.useGlobalFilters(new CustomExceptionFilter)
   app.enableCors();
   const configService = app.get(ConfigService)
   await app.listen(configService.get('nest_server_port'));
